79cee6b5ae1e54b4259810af6bd6e9e23abeeaab
Nick Grealy <nickgrealy@gmail.com>
2020-10-09 09:26:35 +1100

    Updates

---
+++ precache-manifest.0c6c39674ddfc1dd1db50b9033bed0e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a502451ef7e8b463d3acb80e614e4c1c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d77b35f773fae0b4b24c",
+    "url": "./static/js/2.e0680494.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e0680494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc4966aeb89d12eab038",
+    "url": "./static/js/main.6f6d441f.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.6f6d441f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68553aeec9ee1bf9fc44",
+    "url": "./static/js/runtime-main.07eed8be.js"
+  }
+]);
